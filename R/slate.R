bd2c6e32cdf6fd90b5b753985f70afd0db63eccb
kevinykuo <kevinykuo@gmail.com>
2021-01-24 10:46:27 -0800

    only support redis

--- R/slate.R
+++ R/slate.R
@@ -1,18 +1,17 @@
-#' Create a Redis Slate
+#' Create a Slate
 #' @param path Unix domain socket.
 #' @param config List of config options.
 #' @export
-redis_slate <- function(path, config = redis_slate_config()) {
+slate <- function(path, config = slate_config()) {
   new_slate(
     list(
       client = redux::hiredis(path = path),
       config = config
-    ),
-    class = "slate_redis"
+    )
   )
 }
 
-redis_slate_config <- function() {
+slate_config <- function() {
   list(
     app_id = paste0("R_", Sys.getpid()),
     default_level = "INFO"
